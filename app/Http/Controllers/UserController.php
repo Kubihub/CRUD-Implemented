1afb33ac0cfae511c1c5185d34415d4b4b4f71eb
Kubihub <kuburatuadiza5@gmail.com>
2023-10-30 08:40:57 +0000

    first commit for this project, that is implementing CRUD

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rule;
+
+class UserController extends Controller
+{
+        public function login(Request $request){
+            $incomingFields = $request->validate([
+                'loginname'=>'required',
+                'loginpassword'=>'required'
+            ]);
+
+        }
+
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
+    }
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name'=> ['required','min:3', 'max:20'],
+            'email'=> ['required','email',],
+            'password'=> ['required','min:8','max:200']
+        ]);
+
+        // $incomingFields['password']= bcrypt($incomingFields['password']);
+      $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/');
+    }
+}
